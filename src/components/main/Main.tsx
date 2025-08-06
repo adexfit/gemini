6cd90106c1382d043cd7bfcfc945087b8b8e2625
adexfit <y.adeboye@gmail.com>
2025-08-06 10:30:38 +0100

    implemented mouse in and out collapse and extend function for sidebar

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,63 @@
+"use client";
+import React, { useState, useRef, useEffect } from "react";
+
+import { FaPlus } from "react-icons/fa6";
+import { HiMicrophone } from "react-icons/hi2";
+import { IoMdSend } from "react-icons/io";
+
 const Main = () => {
+  const [enteredPrompt, setEnteredPrompt] = useState("");
+
   return (
-    <div>
-      <p>Main</p>
+    <div className="relative flex w-full flex-1 flex-col">
+      <div className="mb-4 flex w-full justify-between p-4">
+        <p className="text-2xl text-gray-500">Gemini</p>
+        <p className="size-4 items-center justify-center rounded-full bg-gray-400 p-4"></p>
+      </div>
+      <div className="absolute bottom-10 left-1/2 w-2/3 -translate-x-1/2">
+        <div className="mb-2 flex-col justify-between rounded-3xl border border-gray-300 px-1 py-4">
+          <div className="flex h-30 w-full flex-col-reverse overflow-hidden">
+            <textarea
+              className="scrollbar-hide w-full flex-grow resize-none overflow-y-auto p-4 outline-none"
+              rows={1}
+              placeholder="Ask Gemini"
+              value={enteredPrompt}
+              onChange={(e) => setEnteredPrompt(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between px-4">
+              <div className="flex items-center gap-x-4 text-sm text-gray-600">
+                <span className="cursor-pointer p-2 hover:rounded-full hover:bg-gray-200">
+                  <FaPlus />
+                </span>
+                <p>
+                  <span>Deep Research</span>
+                </p>
+                <p>
+                  <span>Canvas</span>
+                </p>
+                <p>
+                  <span>Image</span>
+                </p>
+              </div>
+              <div className="cursor-pointer rounded-full bg-gray-100 p-2 hover:bg-gray-300">
+                {enteredPrompt.trim().length > 1 ? (
+                  <IoMdSend />
+                ) : (
+                  <HiMicrophone />
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-sm text-gray-500">
+            Gemini can make mistakes, so double-check it
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,15 +2,12 @@
 import React, { useState, useContext } from "react";
 import { FiMenu } from "react-icons/fi";
 import { FiEdit } from "react-icons/fi";
-import Image from "next/image";
 import { RiSettings2Line } from "react-icons/ri";
 import { MdOutlineSearch } from "react-icons/md";
-import { MdOutlineMenuBook } from "react-icons/md";
-import { on } from "events";
 // import { AiContext } from "../../context/AiContext";
 
 const Sidebar = () => {
-  // const [extended, setExtended] = useState(false);
+  const [extended, setExtended] = useState(false);
   const [keepExtended, setkeepExtended] = useState(false);
   //   const {
   //     setInputedText,
@@ -19,12 +16,6 @@ const Sidebar = () => {
   //     setDetectedLang,
   //     setTranslated,
   //   } = useContext(AiContext);
-
-  // const handleToggle = () => {
-  //   setExtended((prev) => {
-  //     return !prev;
-  //   });
-  // };
 
   const handleNew = () => {
     // setWelcome(true);
@@ -38,6 +29,29 @@ const Sidebar = () => {
     setkeepExtended((prev) => {
       return !prev;
     });
+    if (keepExtended) {
+      setExtended(true);
+    }
+  };
+
+  const handleMouseEnter = () => {
+    console.log("mouse enter");
+    if (keepExtended || extended) {
+      console.log("keep extended or already extended");
+      return;
+    } else {
+      setExtended(true);
+      console.log("now setting extended to true");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    console.log("mouse leave");
+    if (!keepExtended) {
+      setExtended(false);
+    } else {
+      return;
+    }
   };
 
   //   onMouseEnter={() =>
@@ -47,29 +61,27 @@ const Sidebar = () => {
 
   return (
     <div
-      className={`inline-flex h-screen flex-col justify-between bg-[#f1faff] pt-5 pb-5 pl-6 ${keepExtended ? "w-64" : "w-20"}`}
+      className={`inline-flex h-screen flex-col justify-between bg-[#f1faff] pt-5 pb-5 pl-6 ${extended ? "w-64" : "w-18"}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div className="pr-5">
         <div className="mb-10 flex items-center justify-between">
           <span onClick={handleKeepExtended} className="cursor-pointer">
             <FiMenu className="size-4 cursor-pointer text-gray-900" />
           </span>
-          {keepExtended && (
+          {extended && (
             <MdOutlineSearch className="size-5 cursor-pointer text-gray-600" />
           )}
         </div>
 
         <div className="flex items-center gap-x-6" onClick={handleNew}>
           <p>
-            <FiEdit className="size-4 cursor-pointer text-gray-600" />
+            <FiEdit className="size-4 cursor-pointer text-gray-500" />
           </p>
-          {keepExtended ? (
-            <p className="text-sm text-gray-500">New chat</p>
-          ) : (
-            ""
-          )}
+          {extended ? <p className="text-sm text-gray-500">New chat</p> : ""}
         </div>
-        {keepExtended ? (
+        {extended ? (
           <div className="recent">
             <p className="recent-title"></p>
             {/* <div className="recent-entry">
@@ -80,15 +92,14 @@ const Sidebar = () => {
           ""
         )}
       </div>
-      <div className="bottom">
-        <div className="flex cursor-pointer items-center gap-x-6">
-          <p>
-            <RiSettings2Line className="size-4" />
-          </p>
-          {keepExtended ? (
-            <p className="text-sm text-gray-600">Settings and help</p>
-          ) : null}
-        </div>
+
+      <div className="flex cursor-pointer items-center gap-x-6">
+        <p>
+          <RiSettings2Line className="size-4" />
+        </p>
+        {extended ? (
+          <p className="text-sm text-gray-600">Settings and help</p>
+        ) : null}
       </div>
     </div>
   );
